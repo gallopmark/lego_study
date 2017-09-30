030572d18ffffa15e6573822609eef3d1c495a03
gallopMark <xiaoma920406@126.c0m>
2017-09-30 13:35:15 +0800

    New changelist

--- app/src/main/java/com/haoyu/app/adapter/AppCommentAdapter.java
+++ app/src/main/java/com/haoyu/app/adapter/AppCommentAdapter.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import com.haoyu.app.basehelper.BaseArrayRecyclerAdapter;
 import com.haoyu.app.entity.CommentEntity;
 import com.haoyu.app.imageloader.GlideImgManager;
-import com.haoyu.app.lego.teach.R;
+import com.haoyu.app.lego.student.R;
 import com.haoyu.app.utils.TimeUtil;
 import com.haoyu.app.view.FullyLinearLayoutManager;
 
