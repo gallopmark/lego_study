402f094f134bbf2b9eeed72484ad48f88eda236c
gallopMark <xiaoma920406@126.com>
2017-10-10 13:54:46 +0800

    commit

--- app/src/main/java/com/haoyu/app/activity/VideoPlayerActivity.java
+++ app/src/main/java/com/haoyu/app/activity/VideoPlayerActivity.java
@@ -395,7 +395,8 @@ public class VideoPlayerActivity extends BaseActivity implements View.OnClickLis
     }
 
     private long seekTime;
-    private int interval;//更新时间间隔
+    private int interval;
+    // 更新时间间隔
 
     private void initContent() {
         mVideoPath = getIntent().getStringExtra("videoUrl");
