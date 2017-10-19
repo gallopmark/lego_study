965a36156df698787e1b8ea851dfa5afc0a2dcaf
gallopMark <xiaoma920406@126.com>
2017-10-19 10:25:09 +0800

    New changelist

--- app/src/main/java/com/haoyu/app/activity/AppHomePageActivity.java
+++ app/src/main/java/com/haoyu/app/activity/AppHomePageActivity.java
@@ -20,7 +20,6 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.haoyu.app.adapter.MyTrainAdapter;
 import com.haoyu.app.adapter.MyTrainCommunityAdapter;
 import com.haoyu.app.adapter.MyTrainCourseListAdapter;
@@ -660,27 +659,21 @@ public class AppHomePageActivity extends BaseActivity implements View.OnClickLis
     }
 
     private void parseCaptureResult(String result) {
-        if (result.startsWith("http") || result.startsWith("https")) {   //如果结果是链接
-            Intent intent = new Intent(context, AppWebViewActivity.class);
-            intent.putExtra("webUrl", result);
-            startActivity(intent);
-        } else {
+        if (result.contains("qtId") && result.contains("service")) {   //扫一扫登录
             try {
                 Gson gson = new Gson();
-                if (gson.fromJson(result, Object.class) instanceof CaptureResult) {
-                    CaptureResult mCaptureResult = gson.fromJson(result, CaptureResult.class);
-                    String qtId = mCaptureResult.getQtId();
-                    String service = mCaptureResult.getService();
-                    String url = Constants.LOGIN_URL;
-                    login(url, qtId, service);
-                } else {
-                    Intent intent = new Intent(context, AppWebViewActivity.class);
-                    intent.putExtra("text", result);
-                    startActivity(intent);
-                }
-            } catch (JsonSyntaxException e) {
-                toast(context, "无法解析扫描结果");
+                CaptureResult mCaptureResult = gson.fromJson(result, CaptureResult.class);
+                String qtId = mCaptureResult.getQtId();
+                String service = mCaptureResult.getService();
+                String url = Constants.LOGIN_URL;
+                login(url, qtId, service);
+            } catch (Exception e) {
+                toast(context, "解析二维码失败");
             }
+        } else if ((result.startsWith("http") || result.startsWith("https")) && result.contains(Constants.REFERER)) {
+            signedOn(result);
+        } else {
+            showMaterialDialog("提示", "非本应用规定的二维码");
         }
     }
 
@@ -709,6 +702,34 @@ public class AppHomePageActivity extends BaseActivity implements View.OnClickLis
                         toast(context, "验证失败");
                     }
                 });
+    }
+
+    private void signedOn(String url) {
+        addSubscription(OkHttpClientManager.getAsyn(context, url, new OkHttpClientManager.ResultCallback<BaseResponseResult>() {
+            @Override
+            public void onBefore(Request request) {
+                showTipDialog();
+            }
+
+            @Override
+            public void onError(Request request, Exception e) {
+                hideTipDialog();
+                onNetWorkError(context);
+            }
+
+            @Override
+            public void onResponse(BaseResponseResult response) {
+                hideTipDialog();
+                if (response != null && response.getResponseCode() != null && response.getResponseCode().equals("00")) {
+                    toast(context, "签到成功");
+                } else {
+                    if (response != null && response.getResponseMsg() != null)
+                        toast(context, response.getResponseMsg());
+                    else
+                        toast(context, "签到失败");
+                }
+            }
+        }));
     }
 
     @Override
