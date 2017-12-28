d353d47fc1315d343a9bf2aaa7fd5f83af70a545
gallopMark <xiaoma920406@126.com>
2017-12-28 17:04:56 +0800

    New changelist

--- app/src/main/java/com/haoyu/app/view/CircleProgressBar.java
+++ app/src/main/java/com/haoyu/app/view/CircleProgressBar.java
@@ -1,130 +1,158 @@
 package com.haoyu.app.view;
 
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Paint.Style;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.haoyu.app.lego.student.R;
 
 
 public class CircleProgressBar extends View {
-    private int maxProgress = 100;
-    private int progress;
-    private int progressStrokeWidth;
-    /**
-     * 圆环的颜色
-     */
-    private int roundColor;
-
-    /**
-     * 圆环进度的颜色
-     */
-    private int roundProgressColor;
-    /**
-     * 圆环的宽度
-     */
-    private float roundWidth;
-    /**
-     * 进度圆环宽度
-     */
-    private float paintWidth;
+    private float maxProgress = 100;
+    private float currentValue;
+    private int arcColor; //圆环的颜色
+    private int progressColor;  //圆环进度的颜色
+    private float arcWidth;  //圆环的宽度
+    private float progressWidth; //进度圆环宽度
     // 画圆所在的距形区域
-    RectF oval;
-    Paint paint;
+    private RectF bgRect;
+    private Paint allArcPaint;
+    private Paint progressPaint;
+    private float startAngle = -90;
+    private float sweepAngle = 360;
+    private int aniSpeed = 1000;
+
+    public CircleProgressBar(Context context) {
+        super(context, null);
+        initView();
+    }
 
     public CircleProgressBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        // TODO 自动生成的构造函数存根
-        oval = new RectF();
-        paint = new Paint();
-        progressStrokeWidth = Tools.dipToPx(context, 3);
-        TypedArray mTypedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.CircleProgressBar);
+        super(context, attrs, 0);
+        initCofig(context, attrs);
+        initView();
+    }
+
+
+    public CircleProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initCofig(context, attrs);
+        initView();
+    }
+
+    private void initCofig(Context context, AttributeSet attrs) {
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleProgressBar);
         // 获取自定义属性和默认值
-        roundColor = mTypedArray.getColor(
-                R.styleable.CircleProgressBar_CircleColor, Color.RED);
-        roundProgressColor = mTypedArray.getColor(
-                R.styleable.CircleProgressBar_CircleProgressColor, Color.GREEN);
-        roundWidth = mTypedArray.getDimension(
-                R.styleable.CircleProgressBar_CircleWidth, 2);
-        paintWidth = mTypedArray.getDimension(
-                R.styleable.CircleProgressBar_paintWidth, 4);
+        arcColor = mTypedArray.getColor(R.styleable.CircleProgressBar_CircleColor, Color.YELLOW);
+        progressColor = mTypedArray.getColor(R.styleable.CircleProgressBar_CircleProgressColor, Color.GREEN);
+        arcWidth = mTypedArray.getDimension(R.styleable.CircleProgressBar_CircleArcWidth, dipToPx(context, 2));
+        progressWidth = mTypedArray.getDimension(R.styleable.CircleProgressBar_CircleProgressWidth, dipToPx(context, 4));
+        maxProgress = mTypedArray.getFloat(R.styleable.CircleProgressBar_maxValue, 100);
+        float progress = mTypedArray.getFloat(R.styleable.CircleProgressBar_currentValue, 0);
+        startAngle = mTypedArray.getFloat(R.styleable.CircleProgressBar_startAngle, -90);
+        sweepAngle = mTypedArray.getFloat(R.styleable.CircleProgressBar_sweepAngle, 360);
+        aniSpeed = mTypedArray.getInt(R.styleable.CircleProgressBar_aniSpeed, 1000);
+        setMaxProgress(maxProgress);
+        setProgress(progress);
         mTypedArray.recycle();
+    }
+
+    private void initView() {
+        bgRect = new RectF();
+        //整个弧形
+        allArcPaint = new Paint();
+        allArcPaint.setAntiAlias(true);
+        allArcPaint.setStyle(Paint.Style.STROKE);
+        allArcPaint.setStrokeWidth(arcWidth);
+        allArcPaint.setColor(arcColor);
+        allArcPaint.setStrokeCap(Paint.Cap.ROUND);
+        //当前进度的弧形
+        progressPaint = new Paint();
+        progressPaint.setAntiAlias(true);
+        progressPaint.setStyle(Paint.Style.STROKE);
+        progressPaint.setStrokeCap(Paint.Cap.ROUND);
+        progressPaint.setStrokeWidth(progressWidth);
+        progressPaint.setColor(progressColor);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // TODO 自动生成的方法存根
-        super.onDraw(canvas);
-        int width = this.getWidth();
-        int height = this.getHeight();
-
-        if (width != height) {
-            int min = Math.min(width, height);
-            width = min;
-            height = min;
-        }
-        int centre = getWidth() / 2; // 获取圆心的x坐标
-        int radius = (int) (centre - roundWidth / 2); // 圆环的半径
-        paint.setAntiAlias(true); // 设置画笔为抗锯齿
-        // paint.setColor(Color.TRANSPARENT); // 设置画笔颜色
-        paint.setColor(roundColor); // 设置画笔颜色
-        canvas.drawColor(Color.TRANSPARENT); // 白色背景
-        // paint.setStrokeWidth(progressStrokeWidth); // 线宽
-        paint.setStrokeWidth(roundWidth); // 线宽
-        paint.setStyle(Style.STROKE);
-
-//		oval.left = progressStrokeWidth / 2; // 左上角x
-//		oval.top = progressStrokeWidth / 2; // 左上角y
-//		oval.right = width - progressStrokeWidth / 2; // 左下角x
-//		oval.bottom = height - progressStrokeWidth / 2; // 右下角y
-
-        // canvas.drawArc(oval, -90, 360, true, paint); // 绘制白色圆圈，即进度条背景
-        canvas.drawCircle(centre, centre, radius, paint); // 画出圆环
-        // paint.setColor(Color.RED);
-        int paintRadius = (int) (centre - paintWidth / 2); // 圆环的半径
-        paint.setColor(roundProgressColor);
-        paint.setStrokeWidth(paintWidth);
-        // paint.setColor(Color.rgb(0x57, 0x87, 0xb6));
-        oval.left = centre - paintRadius; // 左上角x
-        oval.top = centre - paintRadius; // 左上角y
-        oval.right = centre + paintRadius; // 右下角x
-        oval.bottom = centre + paintRadius; // 右下角y
-        canvas.drawArc(oval, -90, ((float) progress / maxProgress) * 360,
-                false, paint); // 绘制进度圆弧，这里是蓝色
+        int centerX = getWidth() / 2;
+        int paintRadius = (int) (centerX - progressWidth / 2); // 圆环的半径
+        bgRect.left = centerX - paintRadius; // 左上角x
+        bgRect.top = centerX - paintRadius; // 左上角y
+        bgRect.right = centerX + paintRadius; // 右下角x
+        bgRect.bottom = centerX + paintRadius; // 右下角y
+//        //整个弧
+        canvas.drawCircle(centerX, centerX, paintRadius, allArcPaint); // 画出圆环
+        canvas.drawArc(bgRect, startAngle, currentValue, false, progressPaint); // 绘制进度圆弧，这里是蓝色
     }
 
-    public int getMaxProgress() {
+    public float getMaxProgress() {
         return maxProgress;
     }
 
-    public void setMaxProgress(int maxProgress) {
+    public void setMaxProgress(float maxProgress) {
         this.maxProgress = maxProgress;
     }
 
-    public void setProgress(int progress) {
-        this.progress = progress;
-        this.invalidate();
+    public void setProgress(float progress) {
+        setProgress(progress, false);
+    }
+
+    public void setProgress(float progress, boolean animate) {
+        if (progress > maxProgress) {
+            progress = maxProgress;
+        }
+        if (progress < 0) {
+            progress = 0;
+        }
+        currentValue = (progress / maxProgress) * sweepAngle;
+        if (animate) {
+            setAnimation();
+        } else {
+            invalidate();
+        }
+    }
+
+    private void setAnimation() {
+        ValueAnimator animator = ValueAnimator.ofFloat(0, currentValue);
+        animator.setDuration(aniSpeed);
+        animator.setInterpolator(new OvershootInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                currentValue = (float) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        animator.start();
     }
 
     /**
      * 非ＵＩ线程调用
      */
-    public void setProgressNotInUiThread(int progress) {
-        this.progress = progress;
-        this.postInvalidate();
+    public void setProgressNotInUiThread(float progress) {
+        if (progress > maxProgress) {
+            progress = maxProgress;
+        }
+        if (progress < 0) {
+            progress = 0;
+        }
+        currentValue = (progress / maxProgress) * sweepAngle;
+        postInvalidate();
     }
 
-    private static class Tools {
-        public static int dipToPx(Context context, int dip) {
-            return (int) (dip * context.getResources().getDisplayMetrics().density);
-        }
+    private int dipToPx(Context context, int dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal, context.getResources().getDisplayMetrics());
     }
 }
