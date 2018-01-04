d36850647597c450a5e17193a708c967141a0014
gallopMark <xiaoma920406@126.com>
2018-01-04 11:14:20 +0800

    New changelist

--- app/src/main/java/com/haoyu/app/base/LegoApplication.java
+++ app/src/main/java/com/haoyu/app/base/LegoApplication.java
@@ -3,18 +3,22 @@ package com.haoyu.app.base;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Environment;
+import android.util.Log;
 
 import com.baidu.mobstat.StatService;
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 import com.haoyu.app.utils.Constants;
+import com.tencent.bugly.crashreport.CrashReport;
 
 import org.wlf.filedownloader.FileDownloadConfiguration;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import io.reactivex.functions.Consumer;
+import io.reactivex.plugins.RxJavaPlugins;
 import okhttp3.CookieJar;
 
 
@@ -29,7 +33,8 @@ public class LegoApplication extends Application {
         application = this;
         initFileDownloader();
         StatService.start(this);
-//        CrashReport.initCrashReport(this);
+        setRxJavaErrorHandler();
+        CrashReport.initCrashReport(this);
     }
 
     private void initFileDownloader() {
@@ -49,6 +54,18 @@ public class LegoApplication extends Application {
 // 3、使用配置文件初始化FileDownloader
         FileDownloadConfiguration configuration = builder.build();
         org.wlf.filedownloader.FileDownloader.init(configuration);
+    }
+
+    /**
+     * RxJava2 当取消订阅后(dispose())，RxJava抛出的异常后续无法接收(此时后台线程仍在跑，可能会抛出IO等异常),全部由RxJavaPlugin接收，需要提前设置ErrorHandler
+     */
+    private void setRxJavaErrorHandler() {
+        RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.e("application", throwable.getMessage());
+            }
+        });
     }
 
     public static void addActivity(Activity activity) {
